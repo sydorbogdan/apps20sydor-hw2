5f6d3cc9a0edc62ff4e1f5587d548973e7f5d972
Bohdan Sydor <bohdansydor@pop-os.localdomain>
2020-11-23 15:51:04 +0200

    updated linkedlist

--- src/main/java/ua/edu/ucu/collections/Node.java
+++ src/main/java/ua/edu/ucu/collections/Node.java
@@ -1,11 +1,25 @@
 package ua.edu.ucu.collections;
 
-import lombok.Getter;
-import lombok.Setter;
 
 public class Node {
-    @Getter @Setter private Object data;
-    @Getter @Setter private Node next;
+    private Object data;
+    private Node next;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 
     public Node(Object inpData, Node inpNext){
         this.data = inpData;
